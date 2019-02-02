b568a6e0036704213130fc8f34b1ee6bacc68df8
Melissa <melissapotter.student@careerdevs.com>
2019-02-02 16:54:47 +0000

    generators

--- async.js
+++ async.js
@@ -1,76 +1,31 @@
-window.onload = function(){
-    
-    function handleError(jqXHR, textStatus, error){
-        console.log(error);
-    }
   /* global $ */ 
-    $.ajax({
-    type: "GET",
-    url: "AJAX/tweets.json",
-    success:  cbTweets,
-    error: handleError
-});
-
-function cbTweets(data) {
-    console.log(data);
-
-       $.ajax({
-    type: "GET",
-    url: "AJAX/friends.json",
-    success: cbFriends,
-    error: handleError
-    });
+window.onload = function(){
+   
+   genWrap(function* gen(){
+     var tweets = yield $.get("tweets.json");
+     console.log(tweets);
+     var friends = yield $.get("friends.json");
+     console.log(friends);
+     var videos = yield $.get("videos.json");
+     console.log(videos);
+     
+     
+   });
+  function genWrap(generator){
     
+    var gen = generator();
+    
+    function handle(yielded){
+      if(!yielded.done){
+        yielded.value.then(function(data){
+          return handle(gen.next(data));
+        });
       }
-        
-    
-    function cbFriends(data) {
-    console.log(data);
-
-       $.ajax({
-    type: "GET",
-    url: "AJAX/videos.json",
-    success:  function(data){
-        console.log(data);
-        
-    },
-        error: handleError
-    
-    });
     }
-
-  function get(url){
-    return new Promise(function(resolve, reject){
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("GET", url, true);
-      xhttp.onload = function(){
-        if (xhttp.status ==200){
-          resolve(JSON.parse(xhttp.response));
-        } else{
-          reject(xhttp.statusText);
-        }
-        
-        };
-        xhttp.onerror = function(){
-          reject(xhttp.statusText);
-        };
-      xhttp.send();
-    });
+    return handle(gen.next());
   }
-  
-  var promise = get("tweets.json");
-  promise.then(function(tweets){
-    console.log(tweets);
-  });
 
+ 
 };
 
 
-
-
-// READY STATES 
-// 0 - request not initialized
-// 1 - request has been set up
-// 2 - request has been sent
-// 3 - request is in process 
-// 4 - request is complete 
